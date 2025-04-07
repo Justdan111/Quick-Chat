23e8abc7da00358bbd33732614367f5b2337fae1
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-04-07 19:39:27 +0100

    Replace logout confirmation text with a destructive button for improved UI

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,11 +1,10 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export default function Logout() {
   return (
     <div className='flex mx-auto items-center text-center'>
-      <h1 className='text-2xl font-bold text-red-500'>Logout</h1>
-      <p className='text-lg'>Are you sure you want to logout?</p>
-      <p className='text-sm text-muted-foreground'>This will log you out of your account.</p>
+      <Button variant="destructive"> Logout </Button>
       
     </div>
   )
