dfb63825e8228d2ad4c234a87d2960ea2d20547f
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-04-08 23:54:30 +0100

    new

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Settings() {
   return (
     <div className='flex items-center mx-auto '>
-       <div>Update your profile </div>
+      Profile 
     </div>
   )
 }
