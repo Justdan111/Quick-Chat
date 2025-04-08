aec90c4aae50f01ae36fda40ad8fb781e3b80ea3
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-04-08 23:35:35 +0100

    new

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function page() {
-  const [chat, setChat] = useState()
+ 
   return (
     <div className='flex mx-auto items-center '>
        <div>
         <Button variant="default" >New chat</Button>
+
 
        </div>
     </div>
