c563616737bccb66651ac35895a802576ab5f0df
Emmanuel Ngulube <130584678+Justdan111@users.noreply.github.com>
2025-05-01 22:09:01 +0100

    new

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Logout() {
   return (
     <div className='flex mx-auto items-center text-center'>
-      <Button variant="destructive"> Logout </Button>
+      <Button variant="destructive"> Logout your account </Button>
       
     </div>
   )
